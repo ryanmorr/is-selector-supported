e25f9f3ae6bc5c0b15c44139ed95977bb8c74693
Ryan Morr <rm.morr@gmail.com>
2015-04-28 18:37:01 -0400

    Use try/catch around insertRule

--- is-selector-supported.js
+++ is-selector-supported.js
@@ -1,6 +1,19 @@
 (function(win){
-	'use strict';
+    'use strict';
 
-	
-	
+    var doc = win.document,
+    style = doc.createElement('style');
+    doc.head.appendChild(style);
+
+    function isSelectorSupported(selector){
+        try{
+            style.sheet.insertRule(selector + '{}', 0);
+            return true;
+        }catch(e){
+            return false;
+        }
+    }
+
+    win.isSelectorSupported = isSelectorSupported;
+    
 })(this);
